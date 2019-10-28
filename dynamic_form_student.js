a71ad2ac2795d0756a32bb2d9e9bf0a6e9f9039a
= <=>
2019-10-28 22:43:38 +0700

    Add in phone number & empty array with MutilpleChoice

--- dynamic_form_student.js
+++ dynamic_form_student.js
@@ -1,11 +1,37 @@
+/*
+@author: Dung Tran
+@date  : 1 Oct 19
+@define link to sheet & form relate
+@NEED TO CLEAN CODE
+*/
+
 link_Data_base = 'https://docs.google.com/spreadsheets/d/1BO9dr4feFbfD4vAcB-h_ltF9GHKL3pZKhVdtR5O4gEM/edit?usp=sharing';
 sheet_tutor = 'Tutor_register';
 sheet_student = 'Learner_register';
 sheet_email = 'Email_address';
 form_student_id = 'https://docs.google.com/forms/d/1Cit5s4LO12IFWOi910oJuY2SNsTayv5xTJsHv15YtMg/edit';
-//message = "Test";
 ld_email = 'dinhngoc.phuoc@heineken.com';
+none_tutor = ["No Tutor is available now!"];
 
+/**
+ * The event handler triggered when opening the spreadsheet.
+ * @param {Event} e The onOpen event.
+ */
+function onOpen() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var menu = [ 
+    {name: "1. Update Form", functionName: "update_form"},
+    null,
+  ];  
+  ss.addMenu("Admin Tab", menu);
+}
+
+/*
+@author: Dung Tran
+@date  : 1 Oct 19
+@brief : to get num of col by header
+@para  : link to sheet, sheet name, name of header  
+*/
 function getColbyTitle(link, sheetName, colTitle){
   var ss = SpreadsheetApp.openByUrl(link);
   var s = ss.getSheetByName(sheetName);
@@ -20,6 +46,12 @@ function getColbyTitle(link, sheetName, colTitle){
   }
 }
 
+/*
+@author: Dung Tran
+@date  : 1 Oct 19
+@brief : to get data from col by header
+@para  : link to sheet, sheet name, name of header  
+*/
 function getDatabyCol(link, sheetName, colTitle){
   var ss = SpreadsheetApp.openByUrl(link);
   var s = ss.getSheetByName(sheetName);
@@ -37,8 +69,83 @@ function getDatabyCol(link, sheetName, colTitle){
         }
       }
     }
-  } 
-  return coldata;
+  }
+  if (coldata != []) {return coldata}
+  else return false;
+}
+
+/*
+@author: Dung Tran
+@date  : 1 Oct 19
+@brief : to merge data from several cells
+@para  : link: link to sheet,
+         sheetName, 
+blank -> 1
+not blank + not book -> 0
+not blank + book -> 1
+not blank + booked -> 0
+*/
+
+function merge_infor (link, sheetName)
+{
+  var ss = SpreadsheetApp.openByUrl(link);
+  var s = ss.getSheetByName(sheetName);
+  var dataRange = s.getDataRange();
+  var data = dataRange.getValues();
+
+  for (var i = 1; i < data.length ; i++) {
+    //not blank + not book -> 0
+    if (data[i][6].search("Booked by") != (-1)) {continue;}
+    //not blank + booked -> 0
+    if (data[i][6] != "" & data[i][5] =="") {continue;}
+    if (data[i][6] != "" & data[i][5] !="") {data[i][6]="";}
+    data[i][2] = (data[i][2].getDate()) + '/' + (data[i][2].getMonth()+1);
+    for (var j = 1; j <= 5; j++) {
+      if (j==3) {data[i][6] += data[i][j] + ' - ';}
+      else if (j==5) {data[i][6] += data[i][j];} 
+      else {data[i][6] += data[i][j] + ' | ';}
+    }
+    s.getRange(i+1, 7).setValue(data[i][6].trim());
+      // var giasu = data[i][1];
+      // var ngay = data[i][2].getDate()+1;
+      // var thang = data[i][2].getMonth()+1;
+      // var gio_start = data[i][3];
+      // var gio_end = data[i][4];
+      // var lack = data[i][6];
+      // var dau_cach = " | ";
+      // data[i][6] = giasu + dau_cach + ngay +'/'+ thang + dau_cach + gio_start + '-' + gio_end + dau_cach + lack;
+      // s.getRange(i+1, 7).setValue(data[i][6]);
+  }
+  //s.getRange(i+1, 7).setValue(data[i][6]);
+}
+
+/*
+@author: Dung Tran
+@date  : 1 Oct 19
+@brief : to update form by data from sheet
+@para  : none 
+*/
+
+function update_form(){
+  merge_infor(link_Data_base,sheet_tutor);
+  var form_tutor = FormApp.getActiveForm();
+  
+  var tutor = getDatabyCol(link_Data_base,sheet_tutor,"Merge Information");
+ 
+  var form_student = FormApp.openByUrl(form_student_id);
+  var items = form_student.getItems();
+  
+  for (var i=0; i<items.length; i++){
+    if (items[i].getTitle() == "Please Select Your Tutor"){
+      var pList = items[i].asMultipleChoiceItem();
+      if (tutor == false) 
+        {pList.setChoiceValues(none_tutor).setRequired(true);
+          return}
+      else
+        {pList.setChoiceValues(tutor).setRequired(true);
+          break;}      
+    }
+  }
 }
 
 function find_data_lastrow(link, sheetname, col){
@@ -99,11 +206,14 @@ function booking(){
       var subject = "Booking Success!";
       var tutor_booked = data[i][1];//Tutor name
       var tutor_email = find_feedback(link_Data_base,sheet_email,1,tutor_booked,2);//Tutor email
+      var tutor_phone = find_feedback(link_Data_base,sheet_email,1,tutor_booked,4);//Tutor phone
       var to_email = tutor_email + ',' + student_email;
       var cc_email = ld_email;
       var message = 'Dear Mr/Ms. ' + tutor_booked + ',\n\n' +
                     'Please be informed that '+ student_booked + ' has booked your calendar as schedule below:' + '\n' +
                     tutor_booked_infor + '\n' + '\n' +
+
+                    "Tutor's phone: " + tutor_phone + '\n' + '\n' +
 
                     'Please arrange your time to be OTIF.' + '\n' + '\n' + 
                     'Regards,' + '\n' + 'HVBVT English Club';
@@ -135,72 +245,49 @@ function booking(){
   update_form();
 }
 
+
 /*
 @author: Dung Tran
-@date  : 1 Oct 19
-@brief : to merge data from several cells
-@para  : link: link to sheet,
-         sheetName, 
-blank -> 1
-not blank + not book -> 0
-not blank + book -> 1
-not blank + booked -> 0
+@date  : 2 Oct 19
+@brief : to send mail from data
+@para  : link to sheet
+         sheet name
+         reference data 
 */
-
-function merge_infor (link, sheetName)
+function send_mail(link, sheetName, reference)
 {
   var ss = SpreadsheetApp.openByUrl(link);
   var s = ss.getSheetByName(sheetName);
-  var dataRange = s.getDataRange();
+  var dataRange = ss.getDataRange();
   var data = dataRange.getValues();
 
-  for (var i = 1; i < data.length ; i++) {
-    //not blank + booked -> 0
-    if (data[i][6].search("Booked by") != (-1)) {continue;}
-    //not blank + not book -> 0 
-    if (data[i][6] != "" & data[i][5] =="") {continue;}
-    if (data[i][6] != "" & data[i][5] !="") {data[i][6]="";}
-    data[i][2] = (data[i][2].getDate()) + '/' + (data[i][2].getMonth()+1);
-    for (var j = 1; j <= 5; j++) {
-      if (j==3) {data[i][6] += data[i][j] + ' - ';}
-      else if (j==5) {data[i][6] += data[i][j];} 
-      else {data[i][6] += data[i][j] + ' | ';}
+  for (var rowdata = 0; rowdata<data.length; rowdata++){
+    if (data[rowdata][1] == coacher){
+      var email1 = data3[rowdata][3];//coacher
+      var email2 = data3[rowdata][4];//SH
+      var email3 = data3[rowdata][5];//TPM/POD?
+              
+      if (email2 != "" && email3 != "")
+        var email_cc = email2 + "," + email3 + "," + email4;
+              
+      if (email2 == "" && email3 != "")
+        var email_cc = email4 + "," + email3;
+              
+      if (email2 != "" && email3 == "")
+        var email_cc = email2 + "," + email4;
+              
+      if (email2 == "" && email3 == "")
+        var email_cc = email4;
+                
+      MailApp.sendEmail(email1, subject, message, {
+                            cc: email_cc});//gui mail cho hoc vien
+        break;
     }
-    s.getRange(i+1, 7).setValue(data[i][6].trim());
-      // var giasu = data[i][1];
-      // var ngay = data[i][2].getDate()+1;
-      // var thang = data[i][2].getMonth()+1;
-      // var gio_start = data[i][3];
-      // var gio_end = data[i][4];
-      // var lack = data[i][6];
-      // var dau_cach = " | ";
-      // data[i][6] = giasu + dau_cach + ngay +'/'+ thang + dau_cach + gio_start + '-' + gio_end + dau_cach + lack;
-      // s.getRange(i+1, 7).setValue(data[i][6]);
   }
-  //s.getRange(i+1, 7).setValue(data[i][6]);
 }
 
-/*
-@author: Dung Tran
-@date  : 1 Oct 19
-@brief : to update form by data from sheet
-@para  : none 
-*/
-
-function update_form(){
-  merge_infor(link_Data_base,sheet_tutor);
-  var form_tutor = FormApp.getActiveForm();
-  
-  var tutor = getDatabyCol(link_Data_base,sheet_tutor,"Merge Information");
- 
-  var form_student = FormApp.openByUrl(form_student_id);
-  var items = form_student.getItems();
-  
-  for (var i=0; i<items.length; i++){
-    if (items[i].getTitle() == "Please Select Your Tutor"){
-      var pList = items[i].asMultipleChoiceItem();
-      pList.setChoiceValues(tutor).setRequired(true);
-      break;
-    }
-  }
+function abc()
+{
+ var t = MailApp.getRemainingDailyQuota();
+ Browser.msgBox(t);
 }
